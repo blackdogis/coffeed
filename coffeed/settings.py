69f43f67582cd53e31f459b640a46b200fa39bd9
David Black <david@blackdogis.com>
2015-07-24 10:36:21 -0500

    preparing for heroku

--- coffeed/settings.py
+++ coffeed/settings.py
@@ -99,22 +99,15 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-# 'default': {
-#     'ENGINE': 'django.db.backends.sqlite3',
-#     'NAME': os.path.join(MAIN_DIR, 'db.sqlite3'),
-# }
-# }
+# Parse database configuration from $DATABASE_URL
+ import dj_database_url
+ DATABASES['default'] = dj_database_url.config()
 
-# # Parse database configuration from $DATABASE_URL
-# import dj_database_url
-# DATABASES['default'] = dj_database_url.config()
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# # Allow all host headers
-# ALLOWED_HOSTS = ['*']
+# Allow all host headers
+ ALLOWED_HOSTS = ['*']
 
 
 
